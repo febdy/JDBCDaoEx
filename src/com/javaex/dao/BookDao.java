061ead8d4831954a8ffeab634b13f87ed11bc716
febdy <BIT-USER@DESKTOP-81R6F60>
2018-01-17 17:59:57 +0900

    bookDAO, VO added // have errors, should be fixed.

---
+++ src/com/javaex/dao/BookDao.java
@@ -0,0 +1,131 @@
+package com.javaex.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.javaex.vo.BookVo;
+
+public class BookDao {
+	private Connection conn = null;
+	private PreparedStatement pstmt = null;
+	private ResultSet rs = null; // Select 할 때만 필요함.
+
+	public void insertBook(BookVo bVo) {
+		// 0. import java.sql.*;
+
+		try {
+			// 1. JDBC 드라이버 (Oracle) 로딩
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+
+			// 2. Connection 얻어오기
+			String url = "jdbc:oracle:thin:@localhost:1521:xe";
+			conn = DriverManager.getConnection(url, "webdb", "webdb");
+
+			// 3. SQL문 준비 / 바인딩 / 실행
+			String query = "INSERT INTO book VALUES (seq_book_id.nextval, ?, ?, ?, ?)";
+			pstmt = conn.prepareStatement(query);
+			pstmt.setString(1, bVo.getTitle());
+			pstmt.setString(2, bVo.getPubs());
+			pstmt.setString(3, bVo.getPubDate());
+			pstmt.setInt(4, bVo.getAuthorId());
+			pstmt.executeUpdate();
+
+			// 4.결과처리
+
+		} catch (ClassNotFoundException e) {
+			System.out.println("error: 드라이버 로딩 실패 - " + e);
+		} catch (SQLException e) {
+			System.out.println("error:" + e);
+		} finally {
+			// 5. 자원정리
+			try {
+				/*
+				 * if (rs != null) { rs.close(); }
+				 */
+				if (pstmt != null) {
+					pstmt.close();
+				}
+				if (conn != null) {
+					conn.close();
+				}
+			} catch (SQLException e) {
+				System.out.println("error:" + e);
+			}
+		}
+	}
+
+	public List<BookVo> selectBookList() {
+		List<BookVo> bookList = new ArrayList<>();
+		BookVo bVo;
+		// 0. import java.sql.*;
+
+		try {
+			// 1. JDBC 드라이버 (Oracle) 로딩
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+
+			// 2. Connection 얻어오기
+			String url = "jdbc:oracle:thin:@localhost:1521:xe";
+			conn = DriverManager.getConnection(url, "webdb", "webdb");
+
+			// 3. SQL문 준비 / 바인딩 / 실행
+			String query = "SELECT b.book_id, b.title, b.pubs, to_char(b.pub_date, 'YY/MM/DD') pub_date,"
+					+ " b.author_id, a.author_name, a.author_desc " + 
+					" FROM book b, author a " + 
+					" WHERE b.author_id = a.author_id" + 
+					" ORDER BY b.book_id";
+			pstmt = conn.prepareStatement(query);
+			pstmt.executeQuery();
+			rs = pstmt.getResultSet();
+			
+			// 4.결과처리
+			while(rs.next()) {
+				bVo = new BookVo();
+				int bookId = rs.getInt("book_id");
+				String title = rs.getString("title");
+				String pubs = rs.getString("pubs");
+				String pubDate = rs.getString("pub_date");
+				int authorId = rs.getInt("author_id");
+				String authorName = rs.getString("author_name");
+				String authorDesc = rs.getString("author_desc");
+				
+				bVo.setBookId(bookId);
+				bVo.setTitle(title);
+				bVo.setPubs(pubs);
+				bVo.setPubDate(pubDate);
+				bVo.setAuthorId(authorId);
+				bVo.setAuthorName(authorName);
+				bVo.setAuthorDesc(authorDesc);
+				
+				bookList.add(bVo);
+			}
+
+		} catch (ClassNotFoundException e) {
+			System.out.println("error: 드라이버 로딩 실패 - " + e);
+		} catch (SQLException e) {
+			System.out.println("error:" + e);
+		} finally {
+			// 5. 자원정리
+			try {
+				if (rs != null) {
+					rs.close();
+				}
+				if (pstmt != null) {
+					pstmt.close();
+				}
+				if (conn != null) {
+					conn.close();
+				}
+			} catch (SQLException e) {
+				System.out.println("error:" + e);
+			}
+		}
+
+		return bookList;
+	}
+
+}
--- src/com/javaex/main/MainApp.java
+++ src/com/javaex/main/MainApp.java
@@ -3,7 +3,9 @@ package com.javaex.main;
 import java.util.List;
 
 import com.javaex.dao.AuthorDao;
+import com.javaex.dao.BookDao;
 import com.javaex.vo.AuthorVo;
+import com.javaex.vo.BookVo;
 
 public class MainApp {
 	public static void main(String[] args) {
@@ -21,5 +23,17 @@ public class MainApp {
 			System.out.println(aVo.getAuthorId() + " " + aVo.getAuthorName() + " " + aVo.getAuthorDesc());
 		}
 
+		BookVo bVo = new BookVo("aaa");
+		BookDao bDao = new BookDao();
+		bDao.insertBook(bVo);
+
+		List<AuthorVo> bookList = bDao.selectBookList();
+
+		System.out.println(bookList.size() + "건 발견됨.");
+
+		for (int i = 0; i < bookList.size(); i++) {
+			bVo = bookList.get(i);
+			System.out.println(bVo.getAuthorId() + " " + bVo.getAuthorName() + " " + bVo.getAuthorDesc());
+		}
 	}
 }
